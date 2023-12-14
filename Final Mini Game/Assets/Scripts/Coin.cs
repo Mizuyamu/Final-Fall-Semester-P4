9cdf7a4abc9675934b709a76e644136bc6206a5f
Mizuyamu <mizuyamu@gmail.com>
2023-12-14 12:00:31 -0800

    push changes

--- Final Mini Game/Assets/Scripts/Coin.cs
+++ Final Mini Game/Assets/Scripts/Coin.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
-{
+{ 
+    private AudioSource _coinAudio;
+
+    private void Start()
+    {
+        _coinAudio = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player Collected the coin.");
             GameObject.Find("Canvas").GetComponent<UIManager>().UpdateCoinCount();
+            _coinAudio.Play();
             Destroy(this.gameObject);
         }
     }
